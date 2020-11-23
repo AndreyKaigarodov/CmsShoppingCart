b33789eb3a67c3e70f2f88e429b665e879a9f55c
Kaigarodov Andrey <kaigarodov2012@yandex.ru>
2020-11-23 16:12:02 +0500

    products controller index

--- CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CmsShoppingCart.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 namespace CmsShoppingCart.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ProductsController : Controller
     {
-        public IActionResult Index()
+        private readonly CmsShoppingCartContext context;
+        public ProductsController(CmsShoppingCartContext context)
         {
-            return View();
+            this.context = context;
+        }
+
+        //Get /admin/products/
+        public async Task<IActionResult> Index()
+        {
+            return View(await context.Products.OrderByDescending(x => x.Id).Include(x => x.Category).ToArrayAsync());
         }
     }
 }
